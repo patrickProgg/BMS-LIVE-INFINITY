02b1d19b2d77a43cb204c004a46f46101ae96c8c
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-06-01 08:59:03 +0800

    T

--- application/views/login.php
+++ application/views/login.php
@@ -321,7 +321,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MONITORING</title>
+    <title>PATRICK MONITORING</title>
     <link rel="icon" type="image/png" href="<?= base_url('assets/images/loan.png'); ?>">
 </head>
 
